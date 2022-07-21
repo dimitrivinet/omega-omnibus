fb605a69d5fe29d0dc13e3225bea0db790ef095d
Dimitri VINET <dimitri.vinet@edu.devinci.fr>
2022-07-21 19:54:29 +0200

    (wip) playing cards widget (2)

---
+++ frontend/lib/src/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class OOTheme {
+  static ThemeData get theme {
+    TextTheme baseTextTheme = const TextTheme();
+    baseTextTheme = GoogleFonts.anekBanglaTextTheme(baseTextTheme);
+    baseTextTheme = baseTextTheme.apply(
+      bodyColor: const Color.fromARGB(255, 254, 237, 159),
+    );
+
+    return ThemeData(textTheme: baseTextTheme);
+  }
+}
